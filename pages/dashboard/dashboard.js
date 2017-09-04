9cec387415f4e4af71ec91aa8a3b1881534041a0
findstr <findstr@sina.com>
2017-09-04 22:14:01 +0800

    drag smooth

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -5,13 +5,13 @@ Page({
    */
   data: {
     rss:[
-      { title: "你好", 
-        subtitle: "我是副标题", 
+      { title: "你好",
+        subtitle: "我是副标题",
         url: "http://hello",
         style:"left:0px"
       },
       {
-        title: "世界", 
+        title: "世界",
         subtitle: "我是副标题",
         url: "http://world",
         style:"left:0px"
@@ -23,96 +23,142 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
-  touchX:0, 
+  touchX:0,
+  touchIndex:null,
+  itemPos:0,
+	//const
   delwidth:100,
-  touchS: function(e) {
-    console.log("touchS" + e)
-    if (e.touches.length != 1)
-      return ;
-    this.touchX = e.touches[0].clientX
-  },
+  updateSpeed:10,
 
-  touchM: function(e) {
-    console.log("touchM" + e)
-    if (e.touches.length != 1)
-      return ;
-    var moveX = e.touches[0].clientX;
-    var deltaX = moveX - this.touchX;
-    var style = "right:0px"
-    if (deltaX < 0) {//move left
-      deltaX = -deltaX;
-      if (deltaX > this.delwidth)
-        deltaX = this.delwidth;
-      style = "right:" + deltaX + "px"
-    } else if (deltaX > 0) {//move right
-
-    } else {
-      return
-    }
-    var idx = e.target.dataset.index
-    if (idx == undefined)
-      return
+  setPos: function (idx, x) {
+    this.itemPos = x
+    var style = "right:" + x + "px"
     var k = "rss[" + idx.toString() + "].style"
-    console.log(k, style)
     var param = {}
     param[k] = style
     this.setData(param)
   },
 
-  touchE: function(e) {
+	addPos: function(idx, x) {
+		this.itemPos += x
+    if (this.itemPos > this.delwidth)
+      this.itemPos = this.delwidth
+    else if (this.itemPos < 0)
+      this.itemPos = 0
+    this.setPos(idx, this.itemPos)
+	},
 
+
+  resetItem: function(idx) {
+    var pos = this.itemPos;
+    if (this.touchIndex == null)
+      return
+    if (this.touchIndex == idx)
+      return
+    this.setPos(this.touchIndex, 0)
+		this.touchIndex = null
+    this.itemPos = 0
+	},
+
+  updateMove: function() {
+		var dir
+		if (this.itemPos < this.delwidth / 2) {
+			dir = -1
+		} else {
+			dir = 1
+		}
+		this.addPos(this.touchIndex, dir * this.updateSpeed)
+  },
+
+  touchS: function(e) {
+    if (e.touches.length != 1)
+      return ;
+    var idx = e.target.dataset.index
+    if (idx == undefined)
+      return
+		this.resetItem(idx)
+    this.touchX = e.touches[0].clientX
+  },
+
+  touchM: function(e) {
+    if (e.touches.length != 1)
+      return ;
+		var idx = e.target.dataset.index
+    if (idx == undefined)
+      return
+    var moveX = e.touches[0].clientX
+    var deltaX = moveX - this.touchX
+		this.touchX = moveX
+		this.touchIndex = idx
+		this.addPos(idx, -deltaX)
+  },
+
+  touchE: function(e) {
+		if (this.itemPos == 0) {
+			this.touchIndex = null
+			return
+		}
+    var that = this
+    var func = function() {
+      var pos = that.itemPos;
+        if (pos == 0 || that.itemPos == that.delwidth)
+        return
+      that.updateMove()
+      setTimeout(func, 30)
+    }
+		func()
   },
 
 })
