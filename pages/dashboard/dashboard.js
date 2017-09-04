56bf098f87f0ae644de0ff4d3de9093b431192ae
findstr <findstr@sina.com>
2017-09-04 17:43:29 +0800

    now can drag

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,11 +1,22 @@
 // pages/dashboard/dashboard.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
+    rss:[
+      { title: "你好", 
+        subtitle: "我是副标题", 
+        url: "http://hello",
+        style:"left:0px"
+      },
+      {
+        title: "世界", 
+        subtitle: "我是副标题",
+        url: "http://world",
+        style:"left:0px"
+      }
+    ]
   },
 
   /**
@@ -62,5 +73,46 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+
+  touchX:0, 
+  delwidth:100,
+  touchS: function(e) {
+    console.log("touchS" + e)
+    if (e.touches.length != 1)
+      return ;
+    this.touchX = e.touches[0].clientX
+  },
+
+  touchM: function(e) {
+    console.log("touchM" + e)
+    if (e.touches.length != 1)
+      return ;
+    var moveX = e.touches[0].clientX;
+    var deltaX = moveX - this.touchX;
+    var style = "right:0px"
+    if (deltaX < 0) {//move left
+      deltaX = -deltaX;
+      if (deltaX > this.delwidth)
+        deltaX = this.delwidth;
+      style = "right:" + deltaX + "px"
+    } else if (deltaX > 0) {//move right
+
+    } else {
+      return
+    }
+    var idx = e.target.dataset.index
+    if (idx == undefined)
+      return
+    var k = "rss[" + idx.toString() + "].style"
+    console.log(k, style)
+    var param = {}
+    param[k] = style
+    this.setData(param)
+  },
+
+  touchE: function(e) {
+
+  },
+
 })
