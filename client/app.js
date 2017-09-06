a8102197cd76e30f2707b24451fde463639eff47
findstr <findstr@sina.com>
2017-09-06 10:15:53 +0800

    move

--- client/app.js
+++ client/app.js
@@ -9,6 +9,8 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
+        console.log("LoginCode:" + res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -19,6 +21,7 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log(res)
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
