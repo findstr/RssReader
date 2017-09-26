4d54464f39269d7e24a8c997a9f97875170f768a
findstr <findstr@sina.com>
2017-09-26 15:17:37 +0800

    remove debug print

--- client/app.js
+++ client/app.js
@@ -66,7 +66,6 @@ App({
     // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
     wx.getUserInfo({
       success: res => {
-        console.log("XXX")
         // 可以将 res 发送给后台解码出 unionId
         this.globalData.userInfo = res.userInfo
         if (this.userInfoReadyCallback) {
